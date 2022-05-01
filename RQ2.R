cda6946c9e06c3fa702d93f585a22f5a06794bd8
[Brett Weaver] <weave333@umn.edu>
2022-05-01 18:07:57 -0500

    Fixed typo

--- RQ2.R
+++ RQ2.R
@@ -32,6 +32,76 @@ sum_stats_final %>%
     tab_header("Summary Statistics") 
 
 
+##################################################
+##################################################
+##################################################
+##################################################
+
+#DF for visualizing extubation and days until first post-op feeding
+rq2_figure <- df_rq2 %>% 
+  group_by(blind_id, enteral_d_oral_initiate_day, diagnosis, early_extub) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(
+    diagnosis = recode(diagnosis, 'ToF' = 'Tetralogy of Fallout', 'CoA' = 'Coarctation of the Aorta')
+  ) %>% 
+  ggplot(aes(x = enteral_d_oral_initiate_day, y = n, fill = early_extub)) + 
+  geom_bar(stat = "identity",  position = "stack") + 
+  labs(title = "Frequency of Days until Post-Op Oral Feeding\n By Early Extubation Status",
+       y = '# Patients Receiving \nFirst Post-Op Oral Feeding',
+       x = '# of Days Until First Post-Op Oral Feeding') +
+  viridis::scale_fill_viridis(
+    name = 'Early Extubation',
+    discrete = T
+  )
+
+#generate ggplotly for first figure
+ggplotly(rq2_figure)
+
+#DF for visualizing extubation and days until first post-op feeding by diagnosis group
+rq2_figure_diag <- df_rq2 %>% 
+  group_by(blind_id, enteral_d_oral_initiate_day, diagnosis, early_extub) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(
+    diagnosis = recode(diagnosis, 'ToF' = 'Tetralogy of Fallout', 'CoA' = 'Coarctation of the Aorta')
+  ) %>% 
+  ggplot(aes(x = enteral_d_oral_initiate_day, y = n, fill = early_extub)) + 
+  geom_bar(stat = "identity",  position = "stack") + 
+  labs(title = "Frequency of Days until Post-Op Oral Feeding\n By Early Extubation Status and Diagnosis Group",
+       y = '# Patients Receiving \nFirst Post-Op Oral Feeding',
+       x = '# of Days Until First Post-Op Oral Feeding') +
+  theme(plot.title.position = 'plot', 
+        plot.title = element_text(hjust = 5)) +
+  facet_grid(~diagnosis) + 
+  viridis::scale_fill_viridis(
+    name = 'Early Extubation',
+    discrete = T
+  )
+
+#generate ggplotly for second figure
+ggplotly(rq2_figure_diag)
+
+rq2_figure_diag_ext <- df_rq2 %>% 
+  group_by(blind_id, enteral_d_oral_initiate_day, diagnosis, early_extub) %>% 
+  count() %>% 
+  ungroup() %>% 
+  mutate(
+    diagnosis = recode(diagnosis, 'ToF' = 'Tetralogy of Fallout', 'CoA' = 'Coarctation of the Aorta')
+  ) %>% 
+  ggplot(aes(x = enteral_d_oral_initiate_day, y = n)) + 
+  geom_bar(stat = "identity",  position = "stack") + 
+  labs(y = '# Patients Receiving \nFirst Post-Op Oral Feeding',
+       x = '# of Days Until First Post-Op Oral Feeding') +
+  facet_grid(early_extub ~ diagnosis) + 
+  scale_y_continuous(sec.axis = sec_axis(name = 'number of stones')) +
+  viridis::scale_fill_viridis(
+    name = 'Early Extubation',
+    discrete = T
+  )
+
+ggplotly(rq2_figure_diag_ext)
+
 
 sum_stats %>%
   plot_ly(
@@ -65,10 +135,14 @@ rq2_plot
 # print plot as plotly
 ggplotly(rq2_plot)
 
+##################################################
+##################################################
+##################################################
+##################################################
 
 
 
-library(kableExtra)
+
 
 
 #### POISSON ######
@@ -86,5 +160,8 @@ exp(coef(poiss_model)) %>%
   broom::tidy() %>% 
   knitr::kable()
 
+#Relative risk
+exp(confint(poiss_model)) %>% 
+  knitr::kable(caption = "95% Confidence Interval")
 
 ###################
